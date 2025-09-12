junit-team/junit5

Introduce `org.junit.platform.commons.io.Resource`
Resolves #4876.

junit-platform-engine/src/main/java/org/junit/platform/engine/support/discovery/ResourceContainerSelectorResolver.java
@@ -12,9 +12,9 @@ package org.junit.platform.engine.support.discovery;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toSet;
-import static org.junit.platform.commons.support.ReflectionSupport.findAllResourcesInClasspathRoot;
-import static org.junit.platform.commons.support.ReflectionSupport.findAllResourcesInPackage;
-import static org.junit.platform.commons.util.ReflectionUtils.findAllResourcesInModule;
+import static org.junit.platform.commons.support.ResourceSupport.findAllResourcesInClasspathRoot;
+import static org.junit.platform.commons.support.ResourceSupport.findAllResourcesInModule;
+import static org.junit.platform.commons.support.ResourceSupport.findAllResourcesInPackage;
 import static org.junit.platform.engine.support.discovery.ResourceUtils.packageName;
 import static org.junit.platform.engine.support.discovery.SelectorResolver.Resolution.selectors;
 import static org.junit.platform.engine.support.discovery.SelectorResolver.Resolution.unresolved;
@@ -24,7 +24,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
 
-import org.junit.platform.commons.support.Resource;
+import org.junit.platform.commons.io.Resource;
+import org.junit.platform.commons.io.ResourceFilter;
 import org.junit.platform.engine.discovery.ClasspathResourceSelector;
 import org.junit.platform.engine.discovery.ClasspathRootSelector;
 import org.junit.platform.engine.discovery.DiscoverySelectors;
@@ -35,10 +36,10 @@ import org.junit.platform.engine.discovery.PackageSelector;
  * @since 1.12
  */
 class ResourceContainerSelectorResolver implements SelectorResolver {
-	private final Predicate<Resource> resourceFilter;
+	private final ResourceFilter resourceFilter;
 
-	ResourceContainerSelectorResolver(Predicate<Resource> resourceFilter, Predicate<String> packageFilter) {
-		this.resourceFilter = packageName(packageFilter).and(resourceFilter);
+	ResourceContainerSelectorResolver(ResourceFilter resourceFilter, Predicate<String> packageFilter) {
+		this.resourceFilter = ResourceFilter.of(packageName(packageFilter).and(resourceFilter::match));
 	}
 
 	@Override
@@ -62,7 +63,7 @@ class ResourceContainerSelectorResolver implements SelectorResolver {
 				.values() //
 				.stream() //
 				.map(LinkedHashSet::new) //
-				.map(DiscoverySelectors::selectClasspathResource) //
+				.map(DiscoverySelectors::selectClasspathResources) //
 				.collect(toSet());
 
 		if (selectors.isEmpty()) {

junit-team/junit-framework

junit-platform-engine/src/main/java/org/junit/platform/engine/support/discovery/ResourceContainerSelectorResolver.java
@@ -12,9 +12,9 @@ package org.junit.platform.engine.support.discovery;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toSet;
-import static org.junit.platform.commons.support.ReflectionSupport.findAllResourcesInClasspathRoot;
-import static org.junit.platform.commons.support.ReflectionSupport.findAllResourcesInPackage;
-import static org.junit.platform.commons.util.ReflectionUtils.findAllResourcesInModule;
+import static org.junit.platform.commons.support.ResourceSupport.findAllResourcesInClasspathRoot;
+import static org.junit.platform.commons.support.ResourceSupport.findAllResourcesInModule;
+import static org.junit.platform.commons.support.ResourceSupport.findAllResourcesInPackage;
 import static org.junit.platform.engine.support.discovery.ResourceUtils.packageName;
 import static org.junit.platform.engine.support.discovery.SelectorResolver.Resolution.selectors;
 import static org.junit.platform.engine.support.discovery.SelectorResolver.Resolution.unresolved;
@@ -24,7 +24,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
 
-import org.junit.platform.commons.support.Resource;
+import org.junit.platform.commons.io.Resource;
+import org.junit.platform.commons.io.ResourceFilter;
 import org.junit.platform.engine.discovery.ClasspathResourceSelector;
 import org.junit.platform.engine.discovery.ClasspathRootSelector;
 import org.junit.platform.engine.discovery.DiscoverySelectors;
@@ -35,10 +36,10 @@ import org.junit.platform.engine.discovery.PackageSelector;
  * @since 1.12
  */
 class ResourceContainerSelectorResolver implements SelectorResolver {
-	private final Predicate<Resource> resourceFilter;
+	private final ResourceFilter resourceFilter;
 
-	ResourceContainerSelectorResolver(Predicate<Resource> resourceFilter, Predicate<String> packageFilter) {
-		this.resourceFilter = packageName(packageFilter).and(resourceFilter);
+	ResourceContainerSelectorResolver(ResourceFilter resourceFilter, Predicate<String> packageFilter) {
+		this.resourceFilter = ResourceFilter.of(packageName(packageFilter).and(resourceFilter::match));
 	}
 
 	@Override
@@ -62,7 +63,7 @@ class ResourceContainerSelectorResolver implements SelectorResolver {
 				.values() //
 				.stream() //
 				.map(LinkedHashSet::new) //
-				.map(DiscoverySelectors::selectClasspathResource) //
+				.map(DiscoverySelectors::selectClasspathResourceByName) //
 				.collect(toSet());
 
 		if (selectors.isEmpty()) {