05c45f91d46eb7a581547fbac6a92eb74cd84853
Marc Philipp <mail@marcphilipp.de>
2025-10-13 10:37:05 +0200

    Simplify tests

--- platform-tests/src/test/java/org/junit/platform/engine/support/hierarchical/ConcurrentHierarchicalTestExecutorServiceTests.java
+++ platform-tests/src/test/java/org/junit/platform/engine/support/hierarchical/ConcurrentHierarchicalTestExecutorServiceTests.java
@@ -10,17 +10,12 @@
 
 package org.junit.platform.engine.support.hierarchical;
 
-import static java.util.Comparator.comparing;
-import static java.util.Comparator.comparingLong;
 import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.Future.State.SUCCESS;
 import static java.util.function.Predicate.isEqual;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.platform.commons.test.PreconditionAssertions.assertPreconditionViolationFor;
 import static org.junit.platform.commons.util.ExceptionUtils.throwAsUncheckedException;
-import static org.junit.platform.engine.support.hierarchical.Node.ExecutionMode.CONCURRENT;
-import static org.junit.platform.engine.support.hierarchical.Node.ExecutionMode.SAME_THREAD;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
@@ -31,15 +26,10 @@ import static org.mockito.Mockito.when;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.time.Instant;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.ToIntFunction;
-import java.util.stream.Stream;
 
 import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
@@ -47,13 +37,16 @@ import org.junit.jupiter.api.AutoClose;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.function.Executable;
-import org.junit.jupiter.api.function.ThrowingSupplier;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.platform.commons.util.Preconditions;
 import org.junit.platform.commons.util.ToStringBuilder;
+import org.junit.platform.engine.TestDescriptor;
+import org.junit.platform.engine.UniqueId;
 import org.junit.platform.engine.support.hierarchical.HierarchicalTestExecutorService.TestTask;
 import org.junit.platform.engine.support.hierarchical.Node.ExecutionMode;
+import org.junit.platform.fakes.TestDescriptorStub;
+import org.opentest4j.AssertionFailedError;
 
 /**
  * @since 6.1
@@ -70,7 +63,7 @@ class ConcurrentHierarchicalTestExecutorServiceTests {
 	@EnumSource(ExecutionMode.class)
 	void executesSingleTask(ExecutionMode executionMode) throws Exception {
 
-		var task = TestTaskStub.withoutResult(executionMode);
+		var task = new TestTaskStub(executionMode);
 
 		var customClassLoader = new URLClassLoader(new URL[0], this.getClass().getClassLoader());
 		try (customClassLoader) {
@@ -78,14 +71,17 @@ class ConcurrentHierarchicalTestExecutorServiceTests {
 			service.submit(task).get();
 		}
 
-		assertThat(task.executionThread()).isNotNull().isNotSameAs(Thread.currentThread());
-		assertThat(task.executionThread().getName()).matches("junit-\\d+-worker-1");
-		assertThat(task.executionThread().getContextClassLoader()).isSameAs(customClassLoader);
+		task.assertExecutedSuccessfully();
+
+		var executionThread = task.executionThread();
+		assertThat(executionThread).isNotNull().isNotSameAs(Thread.currentThread());
+		assertThat(executionThread.getName()).matches("junit-\\d+-worker-1");
+		assertThat(executionThread.getContextClassLoader()).isSameAs(customClassLoader);
 	}
 
 	@Test
 	void invokeAllMustBeExecutedFromWithinThreadPool() {
-		var tasks = List.of(TestTaskStub.withoutResult(CONCURRENT));
+		var tasks = List.of(new TestTaskStub(ExecutionMode.CONCURRENT));
 		service = new ConcurrentHierarchicalTestExecutorService(configuration(1));
 
 		assertPreconditionViolationFor(() -> requiredService().invokeAll(tasks)) //
@@ -98,10 +94,13 @@ class ConcurrentHierarchicalTestExecutorServiceTests {
 			throws Exception {
 		service = new ConcurrentHierarchicalTestExecutorService(configuration(1));
 
-		var child = TestTaskStub.withoutResult(childExecutionMode);
-		var root = TestTaskStub.withoutResult(CONCURRENT, () -> requiredService().invokeAll(List.of(child)));
+		var child = new TestTaskStub(childExecutionMode);
+		var root = new TestTaskStub(ExecutionMode.CONCURRENT, () -> requiredService().invokeAll(List.of(child)));
 
 		service.submit(root).get();
+
+		root.assertExecutedSuccessfully();
+		child.assertExecutedSuccessfully();
 
 		assertThat(root.executionThread()).isNotNull();
 		assertThat(child.executionThread()).isSameAs(root.executionThread());
@@ -112,31 +111,36 @@ class ConcurrentHierarchicalTestExecutorServiceTests {
 		service = new ConcurrentHierarchicalTestExecutorService(configuration(2));
 
 		var latch = new CountDownLatch(2);
-		ThrowingSupplier<Boolean> behavior = () -> {
+		Executable behavior = () -> {
 			latch.countDown();
-			return latch.await(100, TimeUnit.MILLISECONDS);
+			latch.await();
 		};
 
-		var children = List.of(TestTaskStub.withResult(CONCURRENT, behavior),
-			TestTaskStub.withResult(CONCURRENT, behavior));
-		var root = TestTaskStub.withoutResult(CONCURRENT, () -> requiredService().invokeAll(children));
+		var children = List.of(new TestTaskStub(ExecutionMode.CONCURRENT, behavior),
+			new TestTaskStub(ExecutionMode.CONCURRENT, behavior));
+		var root = new TestTaskStub(ExecutionMode.CONCURRENT, () -> requiredService().invokeAll(children));
 
 		service.submit(root).get();
 
-		assertThat(children).extracting(TestTaskStub::result).containsOnly(true);
+		root.assertExecutedSuccessfully();
+		assertThat(children).allSatisfy(TestTaskStub::assertExecutedSuccessfully);
 	}
 
 	@Test
 	void executesTwoChildrenInSameThread() throws Exception {
 		service = new ConcurrentHierarchicalTestExecutorService(configuration(1));
 
-		var children = List.of(TestTaskStub.withoutResult(SAME_THREAD), TestTaskStub.withoutResult(SAME_THREAD));
-		var root = TestTaskStub.withoutResult(CONCURRENT, () -> requiredService().invokeAll(children));
+		var children = List.of(new TestTaskStub(ExecutionMode.SAME_THREAD),
+			new TestTaskStub(ExecutionMode.SAME_THREAD));
+		var root = new TestTaskStub(ExecutionMode.CONCURRENT, () -> requiredService().invokeAll(children));
 
 		service.submit(root).get();
 
 		assertThat(root.executionThread()).isNotNull();
 		assertThat(children).extracting(TestTaskStub::executionThread).containsOnly(root.executionThread());
+
+		root.assertExecutedSuccessfully();
+		assertThat(children).allSatisfy(TestTaskStub::assertExecutedSuccessfully);
 	}
 
 	@Test
@@ -144,12 +148,12 @@ class ConcurrentHierarchicalTestExecutorServiceTests {
 		var resourceLock = mock(ResourceLock.class);
 		when(resourceLock.acquire()).thenReturn(resourceLock);
 
-		var task = TestTaskStub.withoutResult(CONCURRENT).withResourceLock(resourceLock);
+		var task = new TestTaskStub(ExecutionMode.CONCURRENT).withResourceLock(resourceLock);
 
 		service = new ConcurrentHierarchicalTestExecutorService(configuration(1));
 		service.submit(task).get();
 
-		assertThat(task.executionThread()).isNotNull();
+		task.assertExecutedSuccessfully();
 
 		var inOrder = inOrder(resourceLock);
 		inOrder.verify(resourceLock).acquire();
@@ -165,17 +169,18 @@ class ConcurrentHierarchicalTestExecutorServiceTests {
 		when(resourceLock.tryAcquire()).thenReturn(true, false);
 		when(resourceLock.acquire()).thenReturn(resourceLock);
 
-		var child1 = TestTaskStub.withoutResult(CONCURRENT).withResourceLock(resourceLock).withName("child1");
-		var child2 = TestTaskStub.withoutResult(CONCURRENT).withResourceLock(resourceLock).withName("child2");
+		var child1 = new TestTaskStub(ExecutionMode.CONCURRENT).withResourceLock(resourceLock).withName("child1");
+		var child2 = new TestTaskStub(ExecutionMode.CONCURRENT).withResourceLock(resourceLock).withName("child2");
 		var children = List.of(child1, child2);
-		var root = TestTaskStub.withoutResult(SAME_THREAD, () -> requiredService().invokeAll(children)).withName(
+		var root = new TestTaskStub(ExecutionMode.SAME_THREAD, () -> requiredService().invokeAll(children)).withName(
 			"root");
 
 		service.submit(root).get();
 
-		assertThat(root.executionThread()).isNotNull();
+		root.assertExecutedSuccessfully();
+		assertThat(children).allSatisfy(TestTaskStub::assertExecutedSuccessfully);
+
 		assertThat(children).extracting(TestTaskStub::executionThread) //
-				.doesNotContainNull() //
 				.filteredOn(isEqual(root.executionThread())).hasSizeLessThan(2);
 
 		verify(resourceLock, atLeast(2)).tryAcquire();
@@ -190,28 +195,27 @@ class ConcurrentHierarchicalTestExecutorServiceTests {
 		var resourceLock = new SingleLock(exclusiveResource(), new ReentrantLock());
 
 		var latch = new CountDownLatch(3);
-		ThrowingSupplier<Boolean> behavior = () -> {
+		Executable behavior = () -> {
 			latch.countDown();
-			return latch.await(100, TimeUnit.MILLISECONDS);
+			latch.await();
 		};
-		var child1 = TestTaskStub.withResult(CONCURRENT, behavior).withResourceLock(resourceLock).withName("child1");
-		var child2 = TestTaskStub.withoutResult(SAME_THREAD).withResourceLock(resourceLock).withName("child2");
-		var leaf1 = TestTaskStub.withResult(CONCURRENT, behavior).withName("leaf1");
-		var leaf2 = TestTaskStub.withResult(CONCURRENT, behavior).withName("leaf2");
-		var leafs = List.of(leaf1, leaf2);
-		var child3 = TestTaskStub.withoutResult(CONCURRENT, () -> requiredService().invokeAll(leafs)).withName(
+		var child1 = new TestTaskStub(ExecutionMode.CONCURRENT, behavior).withResourceLock(resourceLock).withName(
+			"child1");
+		var child2 = new TestTaskStub(ExecutionMode.SAME_THREAD).withResourceLock(resourceLock).withName("child2");
+		var leaf1 = new TestTaskStub(ExecutionMode.CONCURRENT, behavior).withName("leaf1");
+		var leaf2 = new TestTaskStub(ExecutionMode.CONCURRENT, behavior).withName("leaf2");
+		var leaves = List.of(leaf1, leaf2);
+		var child3 = new TestTaskStub(ExecutionMode.CONCURRENT, () -> requiredService().invokeAll(leaves)).withName(
 			"child3");
 		var children = List.of(child1, child2, child3);
-		var root = TestTaskStub.withoutResult(SAME_THREAD, () -> requiredService().invokeAll(children)).withName(
+		var root = new TestTaskStub(ExecutionMode.SAME_THREAD, () -> requiredService().invokeAll(children)).withName(
 			"root");
 
 		service.submit(root).get();
 
-		assertThat(root.executionThread()).isNotNull();
-		assertThat(children).extracting(TestTaskStub::executionThread).doesNotContainNull();
-		assertThat(leafs).extracting(TestTaskStub::executionThread).doesNotContainNull();
-		assertThat(Stream.concat(Stream.of(child1), leafs.stream())).extracting(TestTaskStub::result) //
-				.containsOnly(true);
+		root.assertExecutedSuccessfully();
+		assertThat(children).allSatisfy(TestTaskStub::assertExecutedSuccessfully);
+		assertThat(leaves).allSatisfy(TestTaskStub::assertExecutedSuccessfully);
 	}
 
 	@Test
@@ -219,20 +223,24 @@ class ConcurrentHierarchicalTestExecutorServiceTests {
 		service = new ConcurrentHierarchicalTestExecutorService(configuration(2));
 
 		var leavesStarted = new CountDownLatch(2);
-		var leaf = TestTaskStub.withoutResult(CONCURRENT, leavesStarted::countDown) //
+		var leaf = new TestTaskStub(ExecutionMode.CONCURRENT, leavesStarted::countDown) //
 				.withName("leaf").withLevel(3);
-		var child1 = TestTaskStub.withoutResult(CONCURRENT, () -> requiredService().submit(leaf).get()) //
+		var child1 = new TestTaskStub(ExecutionMode.CONCURRENT, () -> requiredService().submit(leaf).get()) //
 				.withName("child1").withLevel(2);
-		var child2 = TestTaskStub.withoutResult(CONCURRENT, leavesStarted::countDown) //
+		var child2 = new TestTaskStub(ExecutionMode.CONCURRENT, leavesStarted::countDown) //
 				.withName("child2").withLevel(2);
-		var child3 = TestTaskStub.withoutResult(SAME_THREAD, leavesStarted::await) //
+		var child3 = new TestTaskStub(ExecutionMode.SAME_THREAD, leavesStarted::await) //
 				.withName("child3").withLevel(2);
 
-		var root = TestTaskStub.withoutResult(SAME_THREAD,
+		var root = new TestTaskStub(ExecutionMode.SAME_THREAD,
 			() -> requiredService().invokeAll(List.of(child1, child2, child3))) //
-				.withName("root").withLevel(1);
+					.withName("root").withLevel(1);
 
 		service.submit(root).get();
+
+		root.assertExecutedSuccessfully();
+		assertThat(List.of(child1, child2, child3)).allSatisfy(TestTaskStub::assertExecutedSuccessfully);
+		leaf.assertExecutedSuccessfully();
 
 		assertThat(leaf.startTime).isBeforeOrEqualTo(child2.startTime);
 	}
@@ -251,42 +259,40 @@ class ConcurrentHierarchicalTestExecutorServiceTests {
 	}
 
 	@NullMarked
-	private static final class TestTaskStub<T extends @Nullable Object> implements TestTask {
+	private static final class TestTaskStub implements TestTask {
 
 		private final ExecutionMode executionMode;
-		private final ThrowingSupplier<T> behavior;
+		private final Executable behavior;
 
 		private ResourceLock resourceLock = NopLock.INSTANCE;
 		private @Nullable String name;
-
-		private final CompletableFuture<@Nullable T> result = new CompletableFuture<>();
-		private volatile @Nullable Instant startTime;
-		private volatile @Nullable Instant endTime;
-		private volatile @Nullable Thread executionThread;
 		private int level = 1;
 
-		static TestTaskStub<?> withoutResult(ExecutionMode executionMode) {
-			return new TestTaskStub<@Nullable Void>(executionMode, () -> null);
-		}
+		private final CompletableFuture<@Nullable Void> result = new CompletableFuture<>();
+		private volatile @Nullable Instant startTime;
+		private volatile @Nullable Thread executionThread;
 
-		static TestTaskStub<?> withoutResult(ExecutionMode executionMode, Executable executable) {
-			return new TestTaskStub<@Nullable Void>(executionMode, () -> {
-				executable.execute();
-				return null;
+		TestTaskStub(ExecutionMode executionMode) {
+			this(executionMode, () -> {
 			});
 		}
 
-		@SuppressWarnings("SameParameterValue")
-		static <T> TestTaskStub<T> withResult(ExecutionMode executionMode, ThrowingSupplier<T> supplier) {
-			return new TestTaskStub<>(executionMode, supplier);
-		}
-
-		TestTaskStub(ExecutionMode executionMode, ThrowingSupplier<T> behavior) {
+		TestTaskStub(ExecutionMode executionMode, Executable behavior) {
 			this.executionMode = executionMode;
 			this.behavior = behavior;
 		}
 
-		TestTaskStub<T> withResourceLock(ResourceLock resourceLock) {
+		TestTaskStub withName(String name) {
+			this.name = name;
+			return this;
+		}
+
+		TestTaskStub withLevel(int level) {
+			this.level = level;
+			return this;
+		}
+
+		TestTaskStub withResourceLock(ResourceLock resourceLock) {
 			this.resourceLock = resourceLock;
 			return this;
 		}
@@ -314,41 +320,29 @@ class ConcurrentHierarchicalTestExecutorServiceTests {
 		@Override
 		public void execute() {
 			startTime = Instant.now();
-			try {
-				Preconditions.condition(!result.isDone(), "task was already executed");
+			Preconditions.condition(!result.isDone(), "task was already executed");
 
-				executionThread = Thread.currentThread();
-				try {
-					result.complete(behavior.get());
-				}
-				catch (Throwable t) {
-					result.completeExceptionally(t);
-					throw throwAsUncheckedException(t);
-				}
+			executionThread = Thread.currentThread();
+			try {
+				behavior.execute();
+				result.complete(null);
 			}
-			finally {
-				endTime = Instant.now();
+			catch (Throwable t) {
+				result.completeExceptionally(t);
+				throw throwAsUncheckedException(t);
 			}
+		}
+
+		void assertExecutedSuccessfully() {
+			if (result.isCompletedExceptionally()) {
+				throw new AssertionFailedError("Failure during execution", result.exceptionNow());
+			}
+			assertThat(result.state()).isEqualTo(SUCCESS);
 		}
 
 		@Nullable
 		Thread executionThread() {
 			return executionThread;
-		}
-
-		T result() {
-			Preconditions.condition(result.isDone(), "task was not executed");
-			return result.getNow(null);
-		}
-
-		TestTaskStub<T> withName(String name) {
-			this.name = name;
-			return this;
-		}
-
-		TestTaskStub<T> withLevel(int level) {
-			this.level = level;
-			return this;
 		}
 
 		@Override
