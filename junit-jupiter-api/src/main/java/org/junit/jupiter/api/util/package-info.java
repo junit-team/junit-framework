e079fac19c175d459423fcec7f893292e2b38a34
Matthias Bünger <hardcore_army@web.de>
2025-11-07 20:04:48 +0100

    Contribute DefaultLocale and DefaultTimeZone
    JUnit Pioneer is happy to contribute its DefaultLocaleExtension and DefaultTimeZoneExtension to JUnit Jupiter.
    
    closes #4727

---
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/util/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains all files of the DefaultLocaleExtension and DefaultTimeZoneExtension.
+ */
+
+@NullMarked
+package org.junit.jupiter.api.util;
+
+import org.jspecify.annotations.NullMarked;
