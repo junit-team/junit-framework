3225b43615645760061aeb5386d7245440dbe05c
Ogu1208 <kdasunb6@gmail.com>
2025-11-08 22:22:50 +0900

    Add inline value class test
    Signed-off-by: Ogu1208 <kdasunb6@gmail.com>

---
+++ jupiter-tests/src/test/kotlin/org/junit/jupiter/api/kotlin/NullableInlineValueClassTest.kt
@@ -0,0 +1,38 @@
+package org.junit.jupiter.api.kotlin
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.assertNotNull
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class NullableInlineValueClassTest {
+
+    @MethodSource("nullableResultProvider")
+    @ParameterizedTest
+    fun testNullableResult(result: Result<String>?) {
+        assertNotNull(result)
+        assertEquals("test", result.getOrNull())
+    }
+
+    @MethodSource("nullableUserIdProvider")
+    @ParameterizedTest
+    fun testNullableUserId(userId: UserId?) {
+        assertNotNull(userId)
+        assertEquals(999L, userId.value)
+    }
+
+    companion object {
+        @JvmStatic
+        fun nullableResultProvider() =
+            listOf(
+                Arguments.of(Result.success("test"))
+            )
+
+        @JvmStatic
+        fun nullableUserIdProvider() =
+            listOf(
+                Arguments.of(UserId(999L))
+            )
+    }
+}
