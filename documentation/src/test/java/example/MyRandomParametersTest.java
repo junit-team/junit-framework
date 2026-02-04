56caa851ed2864c3fbc245c48716daab9f548b84
Anton.Yalyshev <yalishev.ant@gmail.com>
2026-02-04 22:56:44 +0100

    Extract MyRandomParametersTest to separate files and use local extension
    Use local RandomNumberExtension instead of referencing external
    RandomParametersExtension from junit-examples repository.
    Demonstrate constructor and method parameter injection.
    
    Signed-off-by: Anton.Yalyshev <yalishev.ant@gmail.com>

---
+++ documentation/src/test/java/example/MyRandomParametersTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015-2026 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package example;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import example.extensions.Random;
+
+import org.junit.jupiter.api.Test;
+
+// tag::user_guide[]
+class MyRandomParametersTest {
+
+	MyRandomParametersTest(@Random int randomNumber) {
+		// Use randomNumber in constructor.
+	}
+
+	@Test
+	void injectsInteger(@Random int i, @Random int j) {
+		assertNotEquals(i, j);
+	}
+
+}
+// end::user_guide[]
