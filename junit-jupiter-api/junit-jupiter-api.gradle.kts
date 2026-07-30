ed2e1eedcc99ff4dca1685b9693b7b8dded968ce
M.P. Korstanje <rien.korstanje@gmail.com>
2026-07-30 23:07:47 +0200

    Enable annotation processor for ConfigurationParameter

--- junit-jupiter-api/junit-jupiter-api.gradle.kts
+++ junit-jupiter-api/junit-jupiter-api.gradle.kts
@@ -9,6 +9,8 @@ plugins {
 description = "JUnit Jupiter API"
 
 dependencies {
+	annotationProcessor(projects.junitPlatformConfigurationProcessor)
+
 	api(platform(projects.junitBom))
 	api(libs.opentest4j)
 	api(projects.junitPlatformCommons)
@@ -37,7 +39,7 @@ eclipseConventions {
 
 tasks {
 	compileJava {
-		options.compilerArgs.add("-Xlint:-module") // due to qualified exports
+		options.compilerArgs.add("-Xlint:-module,-processing") // due to qualified exports, not all annotations need to be processed
 	}
 	jar {
 		bundle {
--- junit-platform-configuration-processor/src/main/java/org/junit/platform/configuration/processor/ConfigurationMetadataAnnotationProcessor.java
+++ junit-platform-configuration-processor/src/main/java/org/junit/platform/configuration/processor/ConfigurationMetadataAnnotationProcessor.java
@@ -14,11 +14,16 @@ import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
 import org.apiguardian.api.API;
 
 @API(status = API.Status.EXPERIMENTAL)
+@SupportedSourceVersion(SourceVersion.RELEASE_17)
+@SupportedAnnotationTypes("org.junit.platform.suite.api.ConfigurationParameter")
 public class ConfigurationMetadataAnnotationProcessor extends AbstractProcessor {
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
