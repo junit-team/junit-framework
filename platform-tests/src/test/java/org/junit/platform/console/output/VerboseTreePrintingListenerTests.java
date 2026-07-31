a78e11bd530e60c02a9244e9c07ca63f52941725
M.P. Korstanje <rien.korstanje@gmail.com>
2026-07-31 13:21:07 +0200

    Group test operation in prepare-act-assert pattern

--- platform-tests/src/test/java/org/junit/platform/console/output/VerboseTreePrintingListenerTests.java
+++ platform-tests/src/test/java/org/junit/platform/console/output/VerboseTreePrintingListenerTests.java
@@ -54,8 +54,8 @@ class VerboseTreePrintingListenerTests {
 	@Test
 	void executionSkipped() {
 		var engine = new TestDescriptorStub(UniqueId.forEngine("demo-engine"), "%c ool test");
-		listener.testPlanExecutionStarted(testPlan(engine));
 
+		listener.testPlanExecutionStarted(testPlan(engine));
 		listener.executionSkipped(TestIdentifier.from(engine), "Test%ndisabled".formatted());
 
 		assertOutput("""
@@ -74,8 +74,8 @@ class VerboseTreePrintingListenerTests {
 	@Test
 	void reportingEntryPublished() {
 		var engine = new TestDescriptorStub(UniqueId.forEngine("demo-engine"), "%c ool test");
-		listener.testPlanExecutionStarted(testPlan(engine));
 
+		listener.testPlanExecutionStarted(testPlan(engine));
 		listener.reportingEntryPublished(TestIdentifier.from(engine), ReportEntry.from("foo", "bar"));
 
 		assertOutput("""
@@ -88,8 +88,8 @@ class VerboseTreePrintingListenerTests {
 	@Test
 	void fileEntryPublished() {
 		var engine = new TestDescriptorStub(UniqueId.forEngine("demo-engine"), "%c ool test");
-		listener.testPlanExecutionStarted(testPlan(engine));
 
+		listener.testPlanExecutionStarted(testPlan(engine));
 		listener.fileEntryPublished(TestIdentifier.from(engine), FileEntry.from(Path.of("test.txt"), "text/plain"));
 
 		assertOutput("""
@@ -102,11 +102,10 @@ class VerboseTreePrintingListenerTests {
 	@Test
 	void executionFinishedWithFailure() {
 		var engine = new TestDescriptorStub(UniqueId.forEngine("demo-engine"), "%c ool test");
-		listener.testPlanExecutionStarted(testPlan(engine));
 		var testIdentifier = TestIdentifier.from(engine);
 
+		listener.testPlanExecutionStarted(testPlan(engine));
 		when(clock.instant()).thenReturn(Instant.EPOCH, Instant.EPOCH.plusMillis(42));
-
 		listener.executionStarted(testIdentifier);
 		listener.executionFinished(testIdentifier, failed(new AssertionError("Boom!")));
 
@@ -127,11 +126,10 @@ class VerboseTreePrintingListenerTests {
 	@Test
 	void failureMessageWithFormatSpecifier() {
 		var engine = new TestDescriptorStub(UniqueId.forEngine("demo-engine"), "%c ool test");
-		listener.testPlanExecutionStarted(testPlan(engine));
 		var testIdentifier = TestIdentifier.from(engine);
 
+		listener.testPlanExecutionStarted(testPlan(engine));
 		when(clock.instant()).thenReturn(Instant.EPOCH, Instant.EPOCH.plusMillis(42));
-
 		listener.executionStarted(testIdentifier);
 		listener.executionFinished(testIdentifier, failed(new AssertionError("%crash")));
 
@@ -155,11 +153,11 @@ class VerboseTreePrintingListenerTests {
 		var test = new TestDescriptorStub(container.getUniqueId().append("method", "demoTest()"), "demoTest()");
 		engine.addChild(container);
 		container.addChild(test);
-		listener.testPlanExecutionStarted(testPlan(engine));
 		var engineIdentifier = TestIdentifier.from(engine);
 		var containerIdentifier = TestIdentifier.from(container);
 		var testIdentifier = TestIdentifier.from(test);
 
+		listener.testPlanExecutionStarted(testPlan(engine));
 		when(clock.instant()).thenReturn( //
 			Instant.EPOCH, // engine started
 			Instant.EPOCH, // container started
@@ -167,7 +165,6 @@ class VerboseTreePrintingListenerTests {
 			Instant.EPOCH.plusMillis(7), // test finished
 			Instant.EPOCH.plusMillis(10), // container finished
 			Instant.EPOCH.plusMillis(15)); // engine finished
-
 		listener.executionStarted(engineIdentifier);
 		listener.executionStarted(containerIdentifier);
 		listener.executionStarted(testIdentifier);
@@ -200,10 +197,9 @@ class VerboseTreePrintingListenerTests {
 		engine.addChild(second);
 		first.addChild(new TestDescriptorStub(first.getUniqueId().append("method", "firstTest()"), "firstTest()"));
 		second.addChild(new TestDescriptorStub(second.getUniqueId().append("method", "secondTest()"), "secondTest()"));
+
 		listener.testPlanExecutionStarted(testPlan(engine));
-
 		when(clock.instant()).thenReturn(Instant.EPOCH);
-
 		listener.executionStarted(TestIdentifier.from(engine));
 		listener.executionStarted(TestIdentifier.from(first));
 		listener.executionStarted(TestIdentifier.from(second));
@@ -224,16 +220,15 @@ class VerboseTreePrintingListenerTests {
 		var fast = new TestDescriptorStub(engine.getUniqueId().append("method", "fast()"), "fast()");
 		engine.addChild(slow);
 		engine.addChild(fast);
-		listener.testPlanExecutionStarted(testPlan(engine));
 		var slowIdentifier = TestIdentifier.from(slow);
 		var fastIdentifier = TestIdentifier.from(fast);
 
+		listener.testPlanExecutionStarted(testPlan(engine));
 		when(clock.instant()).thenReturn( //
 			Instant.EPOCH, // slow started
 			Instant.EPOCH.plusMillis(100), // fast started
 			Instant.EPOCH.plusMillis(300), // fast finished -> 200 ms
 			Instant.EPOCH.plusMillis(700)); // slow finished -> 700 ms
-
 		listener.executionStarted(slowIdentifier);
 		listener.executionStarted(fastIdentifier);
 		listener.executionFinished(fastIdentifier, successful());
@@ -270,8 +265,8 @@ class VerboseTreePrintingListenerTests {
 				.toList();
 		testDescriptors.forEach(engine::addChild);
 		var identifiers = testDescriptors.stream().map(TestIdentifier::from).toList();
-		listener.testPlanExecutionStarted(testPlan(engine));
 
+		listener.testPlanExecutionStarted(testPlan(engine));
 		when(clock.instant()).thenReturn(Instant.EPOCH);
 
 		var barrier = new CyclicBarrier(threadCount);
